8b8bd359609387cbfacfaa848b3648d7d0f18613
viktor <viktor.bulkin@stremline-c.com>
2022-09-09 17:52:20 +0300

    add field validation

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,97 @@
 
     <div v-show="page === 1" class="page">
       <h6 class="mb-4">Информация об организаторе</h6>
-      <form>
+      <form novalidate>
         <div class="mb-3">
-          <label for="organization" class="form-label">Организатор</label>
-          <input type="text" v-model="formReg.organization" class="form-control" id="organization" placeholder="Coca-Cola">
+          <label for="organization" class="form-label" >Организатор</label>
+          <input 
+              @blur="$v.formReg.organization.$touch()"
+              type="text" 
+              v-model="formReg.organization" 
+              class="form-control"
+              :class="{'is-invalid':$v.formReg.organization.$error}" 
+              id="organization" 
+              placeholder="Coca-Cola">
+              <div v-if="!$v.formReg.organization.required" class="invalid-feedback">
+                {{validationMessageRequired}}
+              </div>
+              <div v-if="!$v.formReg.organization.minLength" class="invalid-feedback">
+                {{validationMessageLength4}}
+              </div>
         </div>
         <h6 >Контактные данные</h6>
         <div class="row g-3 mb-4 mt-2">
           <div class="col-md">
             <label for="phone" class="form-label">Телефон</label>
-            <input type="phone" v-model="formReg.phone" class="form-control" id="phone" placeholder="+7 (999) 555-33-22">
+            <input 
+                type="phone"
+                v-model="formReg.phone" 
+                class="form-control"
+                id="phone" 
+                @blur="$v.formReg.phone.$touch()"
+                :class="{'is-invalid':$v.formReg.phone.$error}" 
+                placeholder="+7 (999) 555-33-22">
+              <div v-if="!$v.formReg.phone.required" class="invalid-feedback">
+                {{validationMessageRequired}}
+              </div>
+              <div v-if="!$v.formReg.phone.numeric" class="invalid-feedback">
+                {{validationMessageAlpha}}
+              </div>
+               <div v-if="!$v.formReg.phone.minLength" class="invalid-feedback">
+                {{validationMessageLength11}}
+              </div>
           </div>
           <div class="col-md">
             <label for="email" class="form-label">E-mail</label>
-            <input type="email" v-model="formReg.email" class="form-control" id="email" placeholder="ivanov@mail.ru">
+            <input 
+                type="email" 
+                v-model="formReg.email" 
+                class="form-control" 
+                id="email" 
+                @blur="$v.formReg.email.$touch()"
+                :class="{'is-invalid':$v.formReg.email.$error}" 
+                placeholder="ivanov@mail.ru">
+                <div v-if="!$v.formReg.email.required" class="invalid-feedback">
+                  {{validationMessageRequired}}
+                </div>
+                <div v-if="!$v.formReg.email.email" class="invalid-feedback">
+                  {{validationMessageEmail}}
+                </div>
           </div>
           <div class="col-md">
             <label for="city" class="form-label">Город организатора</label>
-            <input type="text" v-model="formReg.city"  class="form-control" id="city" placeholder="Казань">
+            <input 
+                type="text" 
+                v-model="formReg.city"  
+                class="form-control" 
+                id="city" 
+                @blur="$v.formReg.city.$touch()"
+                :class="{'is-invalid':$v.formReg.city.$error}" 
+                placeholder="Казань">
+                <div v-if="!$v.formReg.city.required" class="invalid-feedback">
+                  {{validationMessageRequired}}
+                </div>
+                <div v-if="!$v.formReg.city.alpha" class="invalid-feedback">
+                  {{validationMessageAlpha}}
+                </div>
           </div>
         </div>
         <h6 class="mb-4">Общая информация</h6>
         <div class="mb-4">
           <label for="name" class="form-label">Название</label>
-          <input type="text"  v-model="formReg.name" class="form-control" id="name">
+          <input 
+              type="text"  
+              v-model="formReg.name" 
+              class="form-control" 
+              @blur="$v.formReg.name.$touch()"
+              :class="{'is-invalid':$v.formReg.name.$error}" 
+              id="name">
+              <div v-if="!$v.formReg.name.required" class="invalid-feedback">
+                {{validationMessageRequired}}
+              </div>
+              <div v-if="!$v.formReg.name.maxLength" class="invalid-feedback">
+                {{validationMessageLength60}}
+              </div>
         </div>
         <h6 class="mb-4">Фотография</h6>
         <div class="d-flex flex-row mb-3">
@@ -45,7 +112,11 @@
         </div>
         <div class="mb-3">
           <label for="description" class="form-label">Подробное описание</label>
-          <textarea class="form-control" id="description" rows="3"  v-model="formReg.description"/>
+          <textarea 
+          class="form-control" 
+          id="description" 
+          rows="3"  
+          v-model="formReg.description"/>
         </div>
         <div class="row g-4 mb-5 mt-2 meeting">
           <div class="col-md">
@@ -105,20 +176,41 @@
         <div class="row g-2 mb-4 mt-5">
           <div class="col-md mb-3">
             <label for="rating" class="form-label">Рейтинг мероприятия</label>
-            <select class="form-select" id="rating"  v-model="formReg.rating">
+            <select 
+            class="form-select" 
+            id="rating" 
+            @blur="$v.formReg.rating.$touch()" 
+            :class="{'is-invalid':$v.formReg.rating.$error}" 
+            v-model="formReg.rating">
               <option value="18" selected>18+</option>
               <option value="0">0+</option>
               <option value="3">3+</option>
               <option value="60">60+</option>
             </select>
+            <div v-if="!$v.formReg.rating.required" class="invalid-feedback">
+                {{validationMessageRequired}}
+              </div>
           </div>
           <div class="col-md">
             <label for="address" class="form-label">Адрес мероприятия</label>
-            <input type="text" class="form-control" id="address"  v-model="formReg.address">
+            <input 
+                type="text" 
+                class="form-control" 
+                id="address"  
+                @blur="$v.formReg.address.$touch()"
+                :class="{'is-invalid':$v.formReg.address.$error}" 
+                v-model="formReg.address">
+              <div v-if="!$v.formReg.address.required" class="invalid-feedback">
+                {{validationMessageRequired}}
+              </div>
           </div>
         </div>
-        <button type="button" class="btn_custom">Отменить</button>
-        <button @click="nextPage" type="button" class="btn_custom">Далее</button>
+        <button @click="cancel" type="button" class="btn_custom">Отменить</button>
+        <!-- :disabled="disabledNextBitton"  -->
+        <button 
+        @click="nextPage"
+        type="button" 
+        class="btn_custom">Далее</button>
       </form>
     </div>
 
@@ -164,11 +256,18 @@
 </template>
 
 <script>
+import { required, minLength, maxLength, between, email, numeric, alpha } from 'vuelidate/lib/validators'
 
 export default {
   data() {
     return {
       page:1,
+      validationMessageRequired: "Поле обязательное для заполнения",
+      validationMessageLength4:"Длинна должна быть не менее 4х символов",
+      validationMessageLength11:"Длинна должна быть не менее 11 символов",
+      validationMessageLength60:"Длинна должна быть не более 60 символов",
+      validationMessageAlpha: "Поле должно состоять только из букв",
+      validationMessageEmail: "Не кореектный адрес электронной почты",
       formReg: {
         organization:'',
         phone:'',
@@ -186,13 +285,68 @@ export default {
       }
     }
   },
+  computed: {
+    disabledNextBitton() {
+          return  this.$v.formReg.organization.$invalid ||
+                  this.$v.formReg.phone.$invalid ||
+                  this.$v.formReg.email.$invalid ||
+                  this.$v.formReg.city.$invalid 
+    }
+  },
   methods: {
     nextPage() {
       this.page++
     },
     prewPage() {
       this.page--
+    },
+    cancel() {
+      for (let input in this.formReg) {
+        this.formReg[input] = ''
+      }
+      this.$v.$reset()
     }
+    
+  },
+  validations: {
+      formReg:{
+        organization: {
+          required,
+          minLength: minLength(4)
+        },
+        phone:{
+          required,
+          numeric,
+          minLength: minLength(10)
+        },
+        email:{
+          required,
+          email
+        },
+        city:{
+          required,
+          alpha
+        },
+        name:{
+          required,
+          maxLength: maxLength(60)
+        },
+        // photo:'',
+        // description:'',
+        // start_date1:'',
+        // start_time1:'',
+        // end_date1:'',
+        // end_time1:'',
+        rating:{
+          required
+        },
+        address:{
+          required
+        },
+        age: {
+          between: between(20, 30)
+        }
+      }
   }
 }
 </script>
